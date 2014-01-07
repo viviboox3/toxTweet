0869d37731cdbdce1ce980a1cd481f31eebd1121
Vivian <hkp233@nyu.edu>
2014-01-06 19:03:47 -0500

    gets tweets and prints!! need to work on rate limiting

--- tweetCatcher.rb
+++ tweetCatcher.rb
@@ -12,20 +12,21 @@ def get_all_tweets(cont)
   client = Twitter::REST::Client.new do |config|
     config.consumer_key = "keJ2l5mveq8uh9eL3zgVQ"
     config.consumer_secret = "iZ9poJ5MEKqv7SzV6M8aMrXgaFMlI0BWA9loDfv4fg"
-    config.access_token = "72216027-PgIkNRazxI5Nu2cCLdrz4zLWH90FFvofnkCJvzEBs"
-    config.access_token_secret = "wTuHEG4J31SBxRjG46jG07FUVOhNcEZVLfXCwoMLoSBTJ"
+    config.access_token = "72216027-XfCkzRRucHLOrEP0pCieIuaReikS5WLEUAJy9cD5G"
+    config.access_token_secret = "eUmxAVeX6rD8TLMcKjEFsSFZEu3n2WreqW4nqIUmqOR20"
   end
 
-=begin
   collect_with_max_id do |max_id|
     options = {:count => 200, :include_rts => true}
     options[:max_id] = max_id unless max_id.nil?
-    client.search(tweet, options).collect do "#{tweet.text}" end
+    client.search(cont, options).each do |result|
+      print_tweet(result)
+    end
   end
-=end
+end
 
-  tweet = client.search(cont, :count =>3, :result_type => "recent").collect
-  puts "#{tweet.text}"
+def print_tweet(result)
+  puts result.text
 end
 
 
